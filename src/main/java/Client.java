98a87d655ea9b64c9a6194888a01a9ed614b372a
MariaMsu <i.knaz@yandex.ru>
2021-03-14 13:27:29 +0300

    draft adding hibernate

---
+++ src/main/java/Client.java
@@ -0,0 +1,55 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "client")
+public class Client {
+    private long client_id;
+    private String client_name;
+    private String phone;
+    private boolean client_is_removed;
+
+    public Client() {
+    }
+
+    public String getClient_name() {
+        return client_name;
+    }
+
+    public void setClient_name(String client_name) {
+        this.client_name = client_name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isClient_is_removed() {
+        return client_is_removed;
+    }
+
+    public void setClient_is_removed(boolean client_is_removed) {
+        this.client_is_removed = client_is_removed;
+    }
+
+    @Id
+    @Column(name = "client_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(long client_id) {
+        this.client_id = client_id;
+    }
+}
+//CREATE TABLE Clients (
+//  client_id SERIAL,
+//  client_name text,
+//  phone text,
+//  client_is_removed boolean DEFAULT false,
+//  PRIMARY KEY (client_id)
+//);
