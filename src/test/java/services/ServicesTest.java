df4328bf88d18bfbfcddc53143242fb24ffba7e2
MariaMsu <i.knaz@yandex.ru>
2021-03-28 15:45:20 +0300

    rewrite tests to run all test by one command

---
+++ src/test/java/services/ServicesTest.java
@@ -0,0 +1,184 @@
+package services;
+
+import dataAccess.client.Client;
+import dataAccess.client.ClientService;
+import dataAccess.film.Film;
+import dataAccess.film.FilmService;
+import dataAccess.order.Order;
+import dataAccess.order.OrderService;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utils.PrepareDatabase;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ServicesTest {
+    private ClientService client_s;
+    private FilmService film_s;
+    private OrderService order_s;
+
+
+    @BeforeClass
+    public void setUp() {
+        this.client_s = new ClientService();
+        this.film_s = new FilmService();
+        this.order_s = new OrderService();
+    }
+
+    @Test(groups = {"basicServiceTest"})
+    public void testClientService() throws IOException, SQLException {
+        PrepareDatabase.initEmptyDB();
+        final long nonExistentId = 100;
+        final long existentId = 1;
+        final Client clientUpdated =
+                new Client("Толстая пчела", "8 800 700-06-11", false);
+        final Client[] dataClients = TablesTestData.getClients();
+
+        // fill the table
+        for (Client client: dataClients) {
+            Assert.assertTrue(client_s.addClient(client));
+        }
+
+        // read the whole table
+        List<Client> clients = client_s.loadAll();
+        Assert.assertEquals(clients.size(), dataClients.length);
+        for (int i =0; i < clients.size(); ++i){
+            Assert.assertEquals(clients.get(i), dataClients[i]);
+        }
+
+        // find by id & update
+        Assert.assertNull(client_s.findClientById(nonExistentId));
+        clientUpdated.setClient_id(nonExistentId);
+        Assert.assertFalse(client_s.updateClient(clientUpdated));
+        clientUpdated.setClient_id(existentId);
+        Assert.assertTrue(client_s.updateClient(clientUpdated));
+        Assert.assertEquals(clientUpdated, client_s.findClientById(existentId));
+
+        // save object with initialized filed "id"
+        Assert.assertFalse(client_s.addClient(clientUpdated));
+
+        // remove all objects from the table
+        for (Client client: clients){
+            Assert.assertTrue(client_s.deleteClientById(client.getClient_id()));
+        }
+    }
+
+    @Test(groups = {"basicServiceTest"})
+    public void testFilmService() throws IOException, SQLException {
+        PrepareDatabase.initEmptyDB();
+        final long nonExistentId = 100;
+        final long existentId = 1;
+        final Film filmUpdated = new Film("Monsters University", "Kori Rae",
+                2013, 2, 3, 1,
+                2, 150, 160, false);
+        final Film[] dataFilms = TablesTestData.getFilms();
+
+        // fill the table
+        for (Film film : dataFilms) {
+            Assert.assertTrue(film_s.addFilm(film));
+        }
+
+        // read the whole table
+        List<Film> films = film_s.loadAll();
+        Assert.assertEquals(films.size(), dataFilms.length);
+        for (int i = 0; i < films.size(); ++i) {
+            Assert.assertEquals(films.get(i), dataFilms[i]);
+        }
+
+        // find by id & update
+        Assert.assertNull(film_s.findFilmById(nonExistentId));
+        filmUpdated.setFilm_id(nonExistentId);
+        Assert.assertFalse(film_s.updateFilm(filmUpdated));
+        filmUpdated.setFilm_id(existentId);
+        Assert.assertTrue(film_s.updateFilm(filmUpdated));
+        Assert.assertEquals(filmUpdated, film_s.findFilmById(existentId));
+
+        // save object with initialized filed "id"
+        Assert.assertFalse(film_s.addFilm(filmUpdated));
+
+        // remove all objects from the table
+        for (Film film : films) {
+            Assert.assertTrue(film_s.deleteFilmById(film.getFilm_id()));
+        }
+    }
+
+    @Test(groups = {"basicServiceTest"}, dependsOnMethods = {"testClientService", "testFilmService"})
+    public void testOrderServiceBasic() throws IOException, SQLException {
+        PrepareDatabase.initEmptyDB();
+        // Orders are related to clients and films.
+        // Therefore, it is required to add values to these two tables
+
+        final Client[] dataClients = TablesTestData.getClients();
+        for (Client client : dataClients) { client_s.addClient(client); }
+        final Film[] dataFilms = TablesTestData.getFilms();
+        for (Film film : dataFilms) { film_s.addFilm(film); }
+
+        final Order[] dataOrders = TablesTestData.getOrders(dataClients, dataFilms);
+        final long nonExistentId = 100;
+        final long existentId = 1;
+        final Order orderUpdated = new Order(dataClients[0], dataFilms[1], "disk", 50,
+                java.sql.Date.valueOf("2008-11-01"), java.sql.Date.valueOf("2008-12-01"));
+
+        // fill the table
+        for (Order order : dataOrders) {
+            Assert.assertTrue(order_s.addOrder(order));
+        }
+
+        // read the whole table
+        List<Order> orders = order_s.loadAll();
+        Assert.assertEquals(orders.size(), dataOrders.length);
+        for (int i = 0; i < orders.size(); ++i) {
+            Assert.assertEquals(orders.get(i), dataOrders[i]);
+        }
+
+        // find by id & update
+        Assert.assertNull(order_s.findOrderById(nonExistentId));
+        orderUpdated.setOrder_id(nonExistentId);
+        Assert.assertFalse(order_s.updateOrder(orderUpdated));
+        orderUpdated.setOrder_id(existentId);
+        Assert.assertTrue(order_s.updateOrder(orderUpdated));
+        Assert.assertEquals(orderUpdated, order_s.findOrderById(existentId));
+
+        // save object with initialized filed "id"
+        Assert.assertFalse(order_s.addOrder(orderUpdated));
+
+        // return Film
+        Assert.assertFalse(order_s.returnFilmByOrderId(1));  // this order is already returned
+        Assert.assertTrue(order_s.returnFilmByOrderId(3));
+
+        // remove all objects from the table
+        for (Order order : orders) {
+            Assert.assertTrue(order_s.deleteOrderById(order.getOrder_id()));
+        }
+    }
+
+    @Test(dependsOnGroups =  {"basicServiceTest"})
+    public void testSpecificOrderMethods() throws IOException, SQLException {
+        PrepareDatabase.initEmptyDB();
+        final Client[] dataClients = TablesTestData.getClients();
+        for (Client client : dataClients) { client_s.addClient(client); }
+        final Film[] dataFilms = TablesTestData.getFilms();
+        for (Film film : dataFilms) { film_s.addFilm(film); }
+        final Order[] dataOrders = TablesTestData.getOrders(dataClients, dataFilms);
+        for (Order order : dataOrders) { order_s.addOrder(order); }
+
+        List<Order> ordersClientInPeriod = order_s.getOrdersOfClientForSpecifiedPeriod(
+                2, java.sql.Date.valueOf("2012-01-01"), java.sql.Date.valueOf("2012-12-31"));
+        Assert.assertEquals(ordersClientInPeriod.size(), 1);
+        Assert.assertEquals(ordersClientInPeriod.get(0), dataOrders[1]);
+
+        List<Order> ordersNotReturned = order_s.getOrdersOfClientNotReturned(2);
+        Assert.assertEquals(ordersNotReturned.size(), 1);
+        Assert.assertEquals(ordersNotReturned.get(0), dataOrders[2]);
+
+        List<Order> ordersFilmInPeriod = order_s.getOrdersOfFilmForSpecifiedPeriod(
+                2, java.sql.Date.valueOf("2012-01-01"), java.sql.Date.valueOf("2012-12-31"));
+        Assert.assertEquals(ordersFilmInPeriod.size(), 1);
+        Assert.assertEquals(ordersFilmInPeriod.get(0), dataOrders[1]);
+
+        PrepareDatabase.initEmptyDB();
+    }
+}
--- src/test/java/services/TablesTestData.java
+++ src/test/java/services/TablesTestData.java
@@ -5,31 +5,37 @@ import dataAccess.film.Film;
 import dataAccess.order.Order;
 
 public class TablesTestData {
-    public static final Client[] clients = {
-            new Client("Лупа", "99-99", false),
-            new Client("Пупа", "66-66", false)
-    };
+    public static Client[] getClients() {
+        return new Client[]{
+                new Client("Лупа", "99-99", false),
+                new Client("Пупа", "66-66", false)
+        };
+    }
 
-    public static final Film[] films = {
-            new Film("The Lord of the Rings 1", "Barrie M. Osborne", 2001,
-                    22, 33, 11,
-                    12, 101, 102, false),
-            new Film("The Lord of the Rings 2", "Barrie M. Osborne", 2002,
-                    22, 33, 11,
-                    12, 101, 102, false),
-            new Film("The Lord of the Rings 3", "Barrie M. Osborne", 2003,
-                    22, 33, 11,
-                    12, 101, 102, false)
-    };
+    public static Film[] getFilms() {
+        return new Film[]{
+                new Film("The Lord of the Rings 1", "Barrie M. Osborne", 2001,
+                        22, 33, 11,
+                        12, 101, 102, false),
+                new Film("The Lord of the Rings 2", "Barrie M. Osborne", 2002,
+                        22, 33, 11,
+                        12, 101, 102, false),
+                new Film("The Lord of the Rings 3", "Barrie M. Osborne", 2003,
+                        22, 33, 11,
+                        12, 101, 102, false)
+        };
+    }
 
-    public static final Order[] orders = {
-            new Order(TablesTestData.clients[1], TablesTestData.films[0], "cassette", 333,
-                    java.sql.Date.valueOf("2011-01-01"), java.sql.Date.valueOf("2011-02-01")),
-            new Order(TablesTestData.clients[1], TablesTestData.films[1], "cassette", 333,
-                    java.sql.Date.valueOf("2012-01-01"), java.sql.Date.valueOf("2012-02-01")),
-            new Order(TablesTestData.clients[1], TablesTestData.films[2], "cassette", 333,
-                    java.sql.Date.valueOf("2013-01-01"), null),
-            new Order(TablesTestData.clients[1], TablesTestData.films[1], "disk", 222,
-                    java.sql.Date.valueOf("2020-11-01"), java.sql.Date.valueOf("2020-12-01")),
-    };
+    public static Order[] getOrders(Client[] clients, Film[] films){
+        return new Order[] {
+                new Order(clients[1], films[0], "cassette", 333,
+                        java.sql.Date.valueOf("2011-01-01"), java.sql.Date.valueOf("2011-02-01")),
+                new Order(clients[1], films[1], "cassette", 333,
+                        java.sql.Date.valueOf("2012-01-01"), java.sql.Date.valueOf("2012-02-01")),
+                new Order(clients[1], films[2], "cassette", 333,
+                        java.sql.Date.valueOf("2013-01-01"), null),
+                new Order(clients[1], films[1], "disk", 222,
+                        java.sql.Date.valueOf("2020-11-01"), java.sql.Date.valueOf("2020-12-01")),
+        };
+    }
 }
