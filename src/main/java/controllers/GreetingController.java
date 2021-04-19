2ce23d7df3341cced5632cb26d331d32fc766392
MariaMsu <i.knaz@yandex.ru>
2021-04-20 00:20:21 +0300

    add draft film page

--- src/main/java/controllers/GreetingController.java
+++ src/main/java/controllers/GreetingController.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import dataAccess.film.Film;
+import dataAccess.film.FilmService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +15,19 @@ public class GreetingController {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @GetMapping("/film")
+    public String filmPage(@RequestParam(name="id", required=true) String filmId, Model model) {
+        model.addAttribute("filmId", filmId);
+        System.out.println("id: " + Long.parseLong(filmId));
+
+        FilmService filmService = new FilmService();
+        Film film = filmService.findFilmById(Long.parseLong(filmId));
+        System.out.println("film: " + film);
+
+        model.addAttribute("filmName", film.getFilm_name());
+        return "film";
     }
 
 }
