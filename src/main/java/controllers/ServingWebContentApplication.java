32966f21b7fff806212a0da6c517a4ad6ee3f869
MariaMsu <i.knaz@yandex.ru>
2021-04-28 13:49:54 +0300

    update Readme

--- src/main/java/controllers/ServingWebContentApplication.java
+++ src/main/java/controllers/ServingWebContentApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServingWebContentApplication {
 
     public static void main(String[] args) {
-        System.setProperty("spring.devtools.restart.enabled", "false");  // todo i don't know meaning of this string
+        // todo i don't know meaning of this string
+        // but without it I can not connect to DB through the web UI
+        System.setProperty("spring.devtools.restart.enabled", "false");
+
         SpringApplication.run(ServingWebContentApplication.class, args);
     }
 }
